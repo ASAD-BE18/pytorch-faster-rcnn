e171bc503c4232e23a5c77be0c49285238a1ff3f
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2023-06-08 14:25:06 +0500

    Update test_config.py

--- config/test_config.py
+++ config/test_config.py
@@ -1,5 +1,5 @@
 class Config:
-    model_weights = "./frcnn_saved/"
+    model_weights = "./frcnn_model/"
     image_path = "/kaggle/input/crop-dataset-coco-format/ml-project-data-annotated-coco/test"
     gpu_id = '2'
     num_classes = 2 + 1
